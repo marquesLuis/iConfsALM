6f04f73509ebe0af3928a1a2a002dbb936866248
marquesLuis <lf.marques@campus.fct.unl.pt>
2013-05-16 11:05:56 +0100

    Feedback Admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  Feedback.create(content: "Aplicacao Fantastica parabens aos alunos!!!")
+  Feedback.create(content: "Aplicacao Mais ou menos :/")
+  Feedback.create(content: "Aplicacao Fraca os alunos deviam chumbar :/")
