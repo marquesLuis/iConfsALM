daf007034bc3fd0abc592c49fe2b8f25a26fb9d9
marquesLuis <lf.marques@campus.fct.unl.pt>
2013-05-31 20:26:30 +0100

    Update Feedbacks:Update

--- app/controllers/update_controller.rb
+++ app/controllers/update_controller.rb
@@ -3,6 +3,14 @@ class UpdateController < ApplicationController
   before_filter :authenticate_registry!
 
   def update
+
+    @update = params[:update]
+
+    @feedbacks = @update[:feedbacks]
+    @feedbacks.each do |fed|
+      Feedback.create(content: fed[:feedback][:content])
+     end
+
   end
 
   def login
