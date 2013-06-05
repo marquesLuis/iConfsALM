d9cecec987512e20f20da79a22ae500d88800965
albertlinde <albert.linde@gmail.com>
2013-06-05 16:18:19 +0100

    User - Paginação na pesquisa.

--- app/controllers/user_search_controller.rb
+++ app/controllers/user_search_controller.rb
@@ -20,25 +20,25 @@ class UserSearchController < ApplicationController
 
   def people_search
     @like = '%'+params[:q]+'%'
-    @people = Person.where("first_name like ? OR last_name like ?", @like, @like)
+    @people = Person.where("first_name like ? OR last_name like ?", @like, @like).paginate(:page => params[:page], :per_page => 6)
 
   end
 
   def events_search
     @like = '%'+params[:q]+'%'
-    @events = Event.where("title like ?", @like)
+    @events = Event.where("title like ?", @like).paginate(:page => params[:page], :per_page => 6)
 
   end
 
   def networking_search
     @like = '%'+params[:q]+'%'
-    @networking = Networking.where("title like ?", @like)
+    @networking = Networking.where("title like ?", @like).paginate(:page => params[:page], :per_page => 6)
 
   end
 
   def notes_search
     @like = '%'+params[:q]+'%'
-    @notes = Note.where("content like ?", @like)
+    @notes = Note.where("content like ?", @like).paginate(:page => params[:page], :per_page => 6)
 
   end
 end
