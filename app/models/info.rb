4a2b4e0a79c74b5caa375af9a42fdbf8e34dd3df
marquesLuis <lf.marques@campus.fct.unl.pt>
2013-06-30 15:05:49 +0100

    Limitar Infos

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,6 +1,12 @@
 class Info < ActiveRecord::Base
   attr_accessible :person_id, :info_type, :value
 
+  PREFIX_TYPES = ['', 'Phone', 'Email', 'Address', 'Facebook', 'LinkedIn', 'Twitter', 'Youtube', 'Site']
+
+  def self.get_types
+    return PREFIX_TYPES
+  end
+
   belongs_to :person, :inverse_of => :infos
 
   validates :info_type, :presence => true
