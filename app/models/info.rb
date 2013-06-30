d08608314cf6760d0560016c0e948dd8a8e58cdf
marquesLuis <lf.marques@campus.fct.unl.pt>
2013-06-30 15:32:33 +0100

    Limitar Infos

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,7 +1,7 @@
 class Info < ActiveRecord::Base
   attr_accessible :person_id, :info_type, :value
 
-  PREFIX_TYPES = ['', 'Phone', 'Email', 'Address', 'Facebook', 'LinkedIn', 'Twitter', 'Youtube', 'Website']
+  PREFIX_TYPES = ['', 'Phone', 'Email', 'Address', 'Facebook', 'LinkedIn', 'Twitter', 'Youtube', 'Website', 'Other']
 
   def self.get_types
     return PREFIX_TYPES
